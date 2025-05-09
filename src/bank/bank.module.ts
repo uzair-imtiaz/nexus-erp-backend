1e15785fe404e6bbcb1f0b35ebb29a84ab0e980a
Uzair Imtiaz <mtu880@gmail.com>
2025-05-09 19:00:05 +0500

    M.T for bank

--- src/bank/bank.module.ts
+++ src/bank/bank.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankController } from './bank.controller';
 import { BankService } from './bank.service';
 import { Bank } from './entity/bank.entity';
+import { TenantModule } from 'src/tenant/tenant.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Bank])],
+  imports: [TypeOrmModule.forFeature([Bank]), TenantModule],
   controllers: [BankController],
   providers: [BankService],
 })
