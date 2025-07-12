a1febda6a28381c779c3430d4fd000ffe0755f4b
Uzair Imtiaz <mtu880@gmail.com>
2025-07-12 17:08:09 +0500

    add scripts for migrations

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,6 +4,7 @@ export default registerAs('database', () => ({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: ['dist/**/**.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
   bigNumberStrings: false,
   synchronize: process.env.NODE_ENV === 'development',
   autoLoadEntities: true,
