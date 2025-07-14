63b1abaea4fa16946f1d9d9635e05c20dec2399e
Uzair Imtiaz <mtu880@gmail.com>
2025-07-14 20:10:58 +0500

    add production  table migration

---
+++ src/data-source.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+});
+
+export default AppDataSource;
